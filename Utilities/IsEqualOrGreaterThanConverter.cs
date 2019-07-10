231a302607d612fd872365c5fe7f0a51406b6924
Dimitrios Karakatsanis <narcolic832@gmail.com>
2019-07-11 00:02:31 +0100

    Items builder

---
+++ Utilities/IsEqualOrGreaterThanConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TFT_Overlay.Utilities
+{
+    public class IsEqualOrGreaterThanConverter : IValueConverter
+    {
+        public static readonly IValueConverter Instance = new IsEqualOrGreaterThanConverter();
+
+        #region Methods...
+
+        #region Convert
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int.TryParse(value.ToString(), out int intValue);
+            int.TryParse(parameter.ToString(), out int compareToValue);
+
+            return intValue >= compareToValue;
+        }
+        #endregion
+
+        #region ConvertBack
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion 
+
+        #endregion
+    }
+}
