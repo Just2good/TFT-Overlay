32bbb30985c0c70ccda900ad370b78a2f34a4f42
hignz <higz-06@hotmail.com>
2019-07-11 20:38:55 +0100

    autoupdater - use wpf messagebox like rest of project

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,28 +1,27 @@
 ﻿using System;
 using System.Net;
 using System.Windows;
-using System.Windows.Forms;
+using TFT_Overlay.Properties;
 
 namespace TFT_Overlay
 {
-
-    public partial class App : System.Windows.Application
+    public partial class App : Application
     {
         private void AutoUpdater(object sender, StartupEventArgs e)
         {
             string currentVersion = Version.version;
             string version;
+
             using (WebClient client = new WebClient())
             {
-                
                 string htmlCode = client.DownloadString("https://raw.githubusercontent.com/Just2Good/TFT-Overlay/master/Version.cs");
                 int versionFind = htmlCode.IndexOf("public static string version = ");
-                version = htmlCode.Substring(versionFind + 32,5);
-                if (currentVersion != version && TFT_Overlay.Properties.Settings.Default.AutoUpdate)
+                version = htmlCode.Substring(versionFind + 32, 5);
+                if (currentVersion != version && Settings.Default.AutoUpdate)
                 {
-                    DialogResult result = System.Windows.Forms.MessageBox.Show("New update available, would you like to download it?", "Confirmation", MessageBoxButtons.YesNo);
+                    var result = MessageBox.Show("New update available, would you like to download it?", "Confirmation", MessageBoxButton.YesNo);
 
-                    if (result == DialogResult.Yes)
+                    if (result == MessageBoxResult.Yes)
                     {
                         try
                         {
@@ -30,18 +29,17 @@ namespace TFT_Overlay
                             ServicePointManager.Expect100Continue = true;
                             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                             client.DownloadFile(new Uri(link), "TFTOverlay.zip");
-                            System.Windows.Forms.MessageBox.Show("The zip file was downloaded to your local directory, please extract and use the updated version instead.", "Downloaded");
+                            MessageBox.Show("The zip file was downloaded to your local directory, please extract and use the updated version instead.", "Downloaded");
                         }
                         catch (WebException ex)
                         {
                             Console.WriteLine(ex.Message);
-                            System.Windows.Forms.MessageBox.Show(ex.Message, "An error occured", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            MessageBox.Show(ex.Message, "An error occured", MessageBoxButton.OK, MessageBoxImage.Error);
                         }
                     }
-                    else if (result == DialogResult.No)
+                    else if (result == MessageBoxResult.No)
                     {
-                        TFT_Overlay.Properties.Settings.Default.AutoUpdate = false;
-                        TFT_Overlay.Properties.Settings.Default.Save();
+                        Settings.SaveSetting("AutoUpdate", false);
                     }
                 }
             }
