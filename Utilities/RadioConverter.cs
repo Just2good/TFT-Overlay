9b1e696d2d0fc00cddfec910077264188dadbc10
hignz <higz-06@hotmail.com>
2019-07-13 03:02:54 +0100

    moved files to Utilities folder

---
+++ Utilities/RadioConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace TFT_Overlay
+{
+    public class RadioConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value.Equals(parameter))
+            {
+                return true;
+            }
+            return false;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value.Equals(true))
+            {
+                return parameter;
+            }
+            return Binding.DoNothing;
+        }
+    }
+
+    public class TabControlRadio : TabControl
+    {
+        public static readonly DependencyProperty RadioSelectionProperty
+            = DependencyProperty.Register("RadioGroup", typeof(string), typeof(TabControlRadio));
+
+        public string RadioSelection
+        {
+            get { return (string)GetValue(RadioSelectionProperty); }
+            set { SetValue(RadioSelectionProperty, value); }
+        }
+    }
+
+}
